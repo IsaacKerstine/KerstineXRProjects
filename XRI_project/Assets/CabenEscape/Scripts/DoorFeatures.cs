30a2da473f2a5d6d412fdc38fa0ea2bc8a83f569
IsaacKerstine <isaackerstine@gmail.com>
2024-02-29 15:21:43 -0500

    More on doors

--- XRI_project/Assets/CabenEscape/Scripts/DoorFeatures.cs
+++ XRI_project/Assets/CabenEscape/Scripts/DoorFeatures.cs
@@ -1,18 +1,89 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class DoorFeatures : MonoBehaviour
+public class DoorFeatures : CoreFeatures
 {
-    // Start is called before the first frame update
-    void Start()
+    //Door Configuration - pivot information, open door state, max angle, Reverse, speed
+    [Header("Door Configurations")]
+    [SerializeField]
+    private Transform doorPivot;
+
+    [SerializeField]
+    private float maxAngle = 90.0f;
+
+    [SerializeField]
+    private bool reverseAngleDirection = false;
+
+    [SerializeField]
+    private float doorSpeed = 1.0f;
+
+    [SerializeField]
+    private bool open = false;
+
+    [SerializeField]
+    private bool MakeKinematicOnOpen = false;
+
+    //Interaction Features for Socket Interactor, Simple Interactor
+    [Header("Interaction Configurations")]
+    [SerializeField]
+    private XRSocketInteractor socketInteractor;
+
+    [SerializeField]
+    private XRSimpleInteractable simpleInteracter;
+
+    private void Start()
     {
-        
+        socketInteractor?.selectEntered.AddListener((s) =>
+        {
+            OpenDoor();
+            PlayOnStart();
+        });
+
+        socketInteractor?.selectExited.AddListener((s) =>
+        {
+            PlayOnEnd();
+            //When we are done exiting, we don't want to reuse the socket
+            socketInteractor.socketActive = featureUsage == FeatureUsage.Once ? false : true;
+        });
+
+        simpleInteracter?.selectEntered.AddListener((s) =>
+        {
+            OpenDoor();
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenDoor()
     {
-        
+        //openDoor ? false : true;
+        if (!open)
+        {
+            PlayOnStart();
+            open = true;
+            StartCoroutine(ProcessMotion());
+        }
+    }
+
+    private IEnumerator ProcessMotion()
+    {
+        var angle = doorPivot.localEulerAngles.y < 180 ? doorPivot.localEulerAngles.y : doorPivot.localEulerAngles.y -360;
+
+        angle = reverseAngleDirection ? Mathf.Abs(angle) : angle;
+        if (angle <= maxAngle)
+        {
+            doorPivot?.Rotate(Vector3.up, doorSpeed * Time.deltaTime * (reverseAngleDirection ? -1 : 1));
+
+        }
+
+        else
+        {
+            open = false;
+            var featureRigidBody = GetComponent<Rigidbody>();
+            if (featureRigidBody != null && MakeKinematicOnOpen) featureRigidBody.isKinematic = true;
+        }
+        yield return null;
     }
 }
